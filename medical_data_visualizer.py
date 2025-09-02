bdc4693ff4fc3785058e8a0978bf3304f0d3fd86
Rebeca Namura Sbroglio <rebeca.sbroglio@sou.inteli.edu.br>
2025-09-02 17:20:09 +0000

    feat: overweight

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,22 +4,28 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv("medical_examination.csv")
 
 # 2
-df['overweight'] = None
+df['overweight'] = ((df['weight']/((df['height']/100))**2)).apply(lambda x: 1 if x > 25 else 0)
+
 
 # 3
+for column in df:
+    df[column] = ((df[column] - df[column].min()) / (df[column].max() - df[column].min())).round(0)
 
 
+df['cholesterol'] = np.where(df['cholesterol'] == 1, 0, 1)
+df['gluc'] = np.where(df['gluc'] == 1, 0, 1)
+print(df)
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
+    df_cat = pd.melt(df, id_vars= ['cardio'], value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
 
 
     # 6
-    df_cat = None
+    df_cat = df_cat.groupby()['cardio']
     
 
     # 7
